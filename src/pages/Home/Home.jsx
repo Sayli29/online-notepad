e1cd6bdb7d246487598a1f755133aabd49705830
Sayli Tawhare <sayli.tawhare@gmail.com>
2023-07-12 16:31:26 +0530

    feat: boilerplate of online-notepad app

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+const Home = () => {
+    return(
+        <div className="hero-container">
+            <span><h1>Start Typing</h1></span>
+            <span><h1>and Sharing Notes..</h1></span>
+            <Button variant="contained" size="large">let's start</Button>
+        </div>
+    );
+}
+
+export default Home;
